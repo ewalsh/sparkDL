c1926625b0cda978f79e354c878f1b0e242dd9ed
ewalsh <ewalsh@brandeis.edu>
2020-06-30 20:45:40 +0100

    page 92

--- src/main/scala/streaming/directKafkaWordCount.scala
+++ src/main/scala/streaming/directKafkaWordCount.scala
@@ -3,7 +3,7 @@ package ai.economicdatasciences.streaming
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010. KafkaUtils
+import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.kafka.clients.consumer.ConsumerRecord
--- src/main/scala/streaming/irisStream.scala
+++ src/main/scala/streaming/irisStream.scala
@@ -4,7 +4,16 @@ import org.apache.spark.SparkConf
 import org.datavec.api.transform.schema.Schema
 import org.datavec.api.transform.TransformProcess
 import org.datavec.api.transform.TransformProcess._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.kafka.clients.consumer.ConsumerRecord
 
+
+import org.apache.kafka.common.serialization.StringDeserializer
 
 object IrisStream {
     def main(args: Array[String]): Unit = {
@@ -24,5 +33,21 @@ object IrisStream {
 
         val sparkConf = new SparkConf().setAppName("DirectKafkaDataVec")
             .setMaster(master)
+        val ssc = new StreamingContext(sparkConf, Seconds(5))
+        val topicsSet = topics.split(" ").toSet
+        val kafkaParams = Map[String, Object](
+          "metadata.broker.list" -> brokers,
+          "bootstrap.servers" -> "localhost:9092",
+          "key.deserializer" -> classOf[StringDeserializer],
+          "value.deserializer" -> classOf[StringDeserializer],
+          "group.id" -> "only",
+          "auto.offset.reset" -> "latest",
+          "enable.auto.commit" -> (false: java.lang.Boolean)
+        )
+
+        val messages = KafkaUtils.createDirectStream(ssc,
+          PreferConsistent,
+          Subscribe[String, String](topicsSet, kafkaParams)
+        )
     }
 }
